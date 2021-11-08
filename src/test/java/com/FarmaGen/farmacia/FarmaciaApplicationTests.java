f5b063c12a80f541213c1ecff1e6282a98c2cd63
LucasAci <lucasqueiroz.aci@gmail.com>
2021-11-08 14:07:47 -0300

    TASK[@lucasaci INITIAL COMMIT]

---
+++ src/test/java/com/FarmaGen/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FarmaGen.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
