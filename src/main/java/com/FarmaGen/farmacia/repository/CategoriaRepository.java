355f496b0c690632b1609c290e43122550bfa0e0
Padilha272 <matheuspadilha48@hotmail.com>
2021-11-08 15:00:29 -0300

    @Padilha272[Task:Create Model and Repository for Table Category]

---
+++ src/main/java/com/FarmaGen/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.FarmaGen.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.FarmaGen.farmacia.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
+
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase (String descricao);
+	
+}
